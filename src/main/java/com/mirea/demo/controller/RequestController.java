6df5aa5625f81be89951169f78434ee101991631
sunnyEmpty <mayak21@ro.ru>
2021-12-11 12:41:12 +0300

    added controller

---
+++ src/main/java/com/mirea/demo/controller/RequestController.java
@@ -0,0 +1,28 @@
+package com.mirea.demo.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/demo")
+@Api(tags = "Методы контроллера")
+@AllArgsConstructor
+public class RequestController {
+    @PostMapping
+    @ApiOperation(value = "Получение post-запроса",
+            notes = "Получение post-запроса")
+    public ResponseEntity<String> createRequest(@RequestBody String requestString) {
+        return ResponseEntity.ok(String.format("Hello %s", requestString));
+    }
+
+    @GetMapping("{name}")
+    @ApiOperation(value = "Получение get-запроса",
+            notes = "Получение данных get-запроса")
+    public ResponseEntity<String> getRequest(@ApiParam("Имя пользователя") @PathVariable String name) {
+        return ResponseEntity.ok(String.format("Hello %s", name));
+    }
+}
